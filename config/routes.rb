9e19521d450cc15c6d784daa71f76408bbd6dd46
Parthey Bhatt <parthey.bhatt@student.adelaide.edu.au>
2019-05-16 18:19:52 +0930

    Added routes for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #To understand how this works, see https://edgeguides.rubyonrails.org/routing.html
+  scope '/admin' do
+    resources :parties do
+      resources :candidates
+    end
+  end
+
+  get '/admin', to: redirect('/admin/parties')
 end
