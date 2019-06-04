cac09c9ba1386e056f1a6c224e28a080799df2c8
a1725532 <max.verhagen@student.adelaide.edu.au>
2019-06-04 15:58:04 +0000

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: "application#index"
   #To understand how this works, see https://edgeguides.rubyonrails.org/routing.html
   scope '/admin' do
     resources :parties do
       resources :candidates
     end
   end
+  
 
   get '/admin', to: redirect('/admin/parties')
+  
 end
