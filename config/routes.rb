7302c46f2aa50e811820434f5002fe926316142f
a1725532 <max.verhagen@student.adelaide.edu.au>
2019-06-04 17:23:55 +0000

    full landing page completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "application#index"
+  scope '/vote' do
+    root to: "application#vote"
+  end 
   #To understand how this works, see https://edgeguides.rubyonrails.org/routing.html
   scope '/admin' do
     resources :parties do
